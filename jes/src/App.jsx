1d3ba8a9a7f6cfce27868f011f3c4b11aeb50ecc
Jes <jesvo5821@gmail.com>
2025-03-29 16:59:21 +1000

    update

--- jes/src/App.jsx
+++ jes/src/App.jsx
@@ -7,13 +7,14 @@ function App() {
     <main>
         <div className={`sct`}>
             <div className={`sct-wlg`}>
-                <div className={`sct-colcs`}>
-                    <p>
+                <div className={`sct-col items-center text-center justify-center h-screen`}>
+                    <p className={`mb-8`}>
                         jes
                     </p>
                     <span className={`material-symbols-outlined`}>
                         deployed_code
                     </span>
+
                 </div>
             </div>
         </div>
